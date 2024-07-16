4398c0b09ec88449c85b7fc9e4859025d901ed37
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2024-07-17 02:06:13 +0300

    Update first_setup.sh

--- first_setup.sh
+++ first_setup.sh
@@ -5,29 +5,33 @@ sudo curl https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chron
 
 # randomize MAC address & disable static hostname
 
-cat > /etc/NetworkManager/conf.d/00-macrandomize.conf <<-'EOF'
-[main]
-hostname-mode=none
+#cat > /etc/NetworkManager/conf.d/00-macrandomize.conf <<-'EOF'
 
-[device]
-wifi.scan-rand-mac-address=yes
+#[main]
+#hostname-mode=none
 
-[connection]
-wifi.cloned-mac-address=random
-ethernet.cloned-mac-address=random
-EOF
+#[device]
+#wifi.scan-rand-mac-address=yes
+
+#[connection]
+#wifi.cloned-mac-address=random
+#ethernet.cloned-mac-address=random
+#EOF
 
 sudo systemctl restart NetworkManager
 #for iwd
+
 cat > /etc/iwd/main.conf  <<-'EOF'
 
 [General]
 AddressRandomization=once
 AddressRandomizationRange=nic
+EOF
 
 #for wpa
 
-sudo bash -c 'cat > /etc/wpa_supplicant/wpa_supplicant-wlan0.conf <<-'EOF'
-mac_addr=1
-preassoc_mac_addr=1
-gas_rand_mac_addr=1
+#cat > /etc/wpa_supplicant/wpa_supplicant-wlan0.conf <<-'EOF'
+#mac_addr=1
+#preassoc_mac_addr=1
+#gas_rand_mac_addr=1
+#EOF
